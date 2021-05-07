5c03ab13810170d088da250bef9fc051db1716e5
Ilya-Sheverov <ilya.sheverov@gmail.com>
2021-05-07 15:22:19 +0300

    Добавлен JavaDoc для класса BeanDefinition.java.

--- src/main/java/ilya/sheverov/dependencyinjectorelinext/bean/BeanDefinition.java
+++ src/main/java/ilya/sheverov/dependencyinjectorelinext/bean/BeanDefinition.java
@@ -3,13 +3,44 @@ package ilya.sheverov.dependencyinjectorelinext.bean;
 import java.lang.reflect.Constructor;
 
 /**
- * Объект этого класса хранит в себе всю необходимую информацию для последующего создания бина.
+ * Объект класса BeanDefinition хранит необходимую информацию для последующего создания бина.
+ * Например, нужно ли создавать бин как синглтон, какой его тип, через какой конструктор его создавать и какие у этого
+ * конструктора параметры.
+ * <p>
+ * Объекты этого классы используются {@link BindingBeansContext} для создания бина.
+ *
+ * @see BindingBeansContext#getBean(Class)
+ * @see BeanDefinitionFactoryImpl
  */
 public class BeanDefinition {
 
+    /**
+     * Указывает на то, требуется ли создавать бин как синглтон.
+     *
+     * @see ilya.sheverov.dependencyinjectorelinext.injector.Injector#bindSingleton(Class, Class)
+     */
     private boolean isSingleton;
+
+    /**
+     * Указывает на то, для какого класса создавать бин.
+     *
+     * @see ilya.sheverov.dependencyinjectorelinext.injector.Injector#bindSingleton(Class, Class)
+     */
     private Class<?> typeOfBean;
+
+    /**
+     * Указывает на то, через какой конструктор создавать бин.
+     * <p>
+     * Конструктор определяется при помощи
+     * {@link ilya.sheverov.dependencyinjectorelinext.сonstructor.determinant.ConstructorDeterminantForInjection }
+     *
+     * @see ilya.sheverov.dependencyinjectorelinext.сonstructor.determinant.ConstructorDeterminantForInjection#determine(Class)
+     */
     private Constructor constructor;
+
+    /**
+     * Параметры конструктора, через который создаётся бин.
+     */
     private Class<?>[] constructorParametersTypes;
 
     public int getConstructorParametersCount() {
